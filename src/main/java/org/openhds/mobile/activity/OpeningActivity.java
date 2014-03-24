74b1f07af23a52dc83725971777e06305846ed45
Motech-Admin <motech-admin@motech-Workstation.(none)>
2014-03-24 17:56:41 -0400

    Refactor OpeningActivity to use framework better.
    Got Fragment animations, preference dialog saved instance state, and much less code.
    
    Conflicts:
    	res/layout/generic_login_fragment.xml
    	res/values/strings.xml

--- src/main/java/org/openhds/mobile/activity/OpeningActivity.java
+++ src/main/java/org/openhds/mobile/activity/OpeningActivity.java
@@ -10,36 +10,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.FrameLayout.LayoutParams;
-import android.widget.LinearLayout;
 
 public class OpeningActivity extends Activity {
 
-	LoginPreferenceFragment loginPrefFrag;
-	FieldWorkerLoginFragment fieldWorkerFrag;
-	SupervisorLoginFragment supervisorFrag;
+	private FrameLayout loginPrefContainer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.opening_activity);
 
-		loginPrefFrag = new LoginPreferenceFragment();
+		loginPrefContainer = (FrameLayout) findViewById(R.id.login_pref_container);
 
-		if (savedInstanceState != null) {
+		if (null != savedInstanceState) {
 			return;
 		}
 
-		fieldWorkerFrag = new FieldWorkerLoginFragment();
-		getFragmentManager().beginTransaction()
-				.add(R.id.field_worker_login_container, fieldWorkerFrag)
-				.commit();
-
-		supervisorFrag = new SupervisorLoginFragment();
-		getFragmentManager().beginTransaction()
-				.add(R.id.supervisor_login_container, supervisorFrag).commit();
-
+		getFragmentManager()
+				.beginTransaction()
+				.add(R.id.login_pref_container, new LoginPreferenceFragment())
+				.add(R.id.field_worker_login_container,
+						new FieldWorkerLoginFragment())
+				.add(R.id.supervisor_login_container,
+						new SupervisorLoginFragment()).commit();
 	}
 
 	@Override
@@ -48,78 +43,17 @@ public class OpeningActivity extends Activity {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.login_menu, menu);
 		return super.onCreateOptionsMenu(menu);
-
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle presses on the action bar items
-
-		if (!loginPrefFrag.isAdded()) {
-			addPreferenceFragment();
-
+		boolean isShowingPreferences = View.VISIBLE == loginPrefContainer
+				.getVisibility();
+		if (isShowingPreferences) {
+			loginPrefContainer.setVisibility(View.GONE);
 		} else {
-			removePreferenceFragment();
+			loginPrefContainer.setVisibility(View.VISIBLE);
 		}
-
 		return true;
 	}
-
-	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState) {
-
-		removePreferenceFragment();
-
-		super.onSaveInstanceState(savedInstanceState);
-	}
-
-	private void addPreferenceFragment() {
-
-		if (!loginPrefFrag.isAdded()) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.login_pref_container, loginPrefFrag, "logintag")
-					.commit();
-			setPreferenceFragmentExpanded(true);
-		}
-
-	}
-
-	private void removePreferenceFragment() {
-
-		if (loginPrefFrag.isAdded()) {
-			getFragmentManager().beginTransaction().remove(loginPrefFrag)
-					.commit();
-			setPreferenceFragmentExpanded(false);
-		}
-
-	}
-
-	private void setPreferenceFragmentExpanded(boolean isExpanded) {
-
-		int height = 0;
-		int width = 0;
-		float weight = isExpanded ? 1 : 0;
-
-		// expand or collapse in portrait mode
-		if (findViewById(R.id.opening_activity).getTag().equals("portrait")) {
-
-			height = 0;
-			width = LayoutParams.MATCH_PARENT;
-
-			// expand or collapse in landscape mode
-		} else if (findViewById(R.id.opening_activity).getTag().equals(
-				"landscape")) {
-
-			height = LayoutParams.MATCH_PARENT;
-			width = 0;
-
-		}
-
-		FrameLayout loginPrefContainer = (FrameLayout) findViewById(R.id.login_pref_container);
-		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(width,
-				height, weight);
-		loginPrefContainer.setLayoutParams(lp);
-
-	}
-
 }
