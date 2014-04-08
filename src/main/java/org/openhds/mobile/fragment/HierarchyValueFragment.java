8e5537f365af58fe15cd2634a266230e35d821d6
Motech-Admin <motech-admin@motech-Workstation.(none)>
2014-04-08 15:27:50 -0400

    Implemented custom ArrayAdapter for HierarchyValueFragment.
    Conflicts:
    	res/layout/listview_value_container.xml

--- src/main/java/org/openhds/mobile/fragment/HierarchyValueFragment.java
+++ src/main/java/org/openhds/mobile/fragment/HierarchyValueFragment.java
@@ -7,6 +7,7 @@ import org.openhds.mobile.activity.HierarchyNavigator;
 import org.openhds.mobile.database.queries.QueryResult;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,11 +17,12 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class HierarchyValueFragment extends Fragment {
 
 	private HierarchyNavigator navigator;
-	ArrayAdapter<QueryResult> queryResultAdapter;
+	HierarchyValueAdapter queryResultAdapter;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,8 +37,9 @@ public class HierarchyValueFragment extends Fragment {
 	}
 
 	public void populateValues(List<QueryResult> values) {
-		queryResultAdapter = new ArrayAdapter<QueryResult>(getActivity(), R.layout.listview_value_container,
-				R.id.listview_text_container, values);
+		queryResultAdapter = new HierarchyValueAdapter(getActivity(), R.layout.listview_value_container,
+				values);
+
 		ListView listView = (ListView) getActivity().findViewById(R.id.value_fragment_listview);
 		listView.setAdapter(queryResultAdapter);
 		listView.setOnItemClickListener(new HierarchyValueListener());
@@ -48,6 +51,32 @@ public class HierarchyValueFragment extends Fragment {
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 			QueryResult selected = queryResultAdapter.getItem(position);
 			navigator.stepDown(selected);
+		}
+	}
+
+	private class HierarchyValueAdapter extends ArrayAdapter<QueryResult> {
+
+		public HierarchyValueAdapter(Context context, int resource, List<QueryResult> objects) {
+			super(context, resource, objects);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.listview_value_container,
+						null);
+			}
+
+			QueryResult qr = queryResultAdapter.getItem(position);
+
+			TextView queryResultName = (TextView) convertView.findViewById(R.id.name_text);
+			queryResultName.setText(qr.getName());
+
+			TextView queryResultExtId = (TextView) convertView.findViewById(R.id.ext_id_text);
+			queryResultExtId.setText(qr.getExtId());
+
+			return convertView;
 		}
 	}
 }
