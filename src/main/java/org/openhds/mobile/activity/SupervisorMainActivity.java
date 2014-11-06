ee1db823dab76ac06aa9664a3d442ab9cea77553
Aurelio Di Pasquale <aurelio.dipasquale@unibas.ch>
2014-11-06 14:37:27 +0100

    v1.2

--- src/main/java/org/openhds/mobile/activity/SupervisorMainActivity.java
+++ src/main/java/org/openhds/mobile/activity/SupervisorMainActivity.java
@@ -22,7 +22,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 public class SupervisorMainActivity extends Activity implements OnClickListener {
 
