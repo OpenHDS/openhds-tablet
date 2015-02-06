518c39e4bf5c9904a37c1ead7807c3c63b6c6cf2
Aurelio Di Pasquale <aurelio.dipasquale@unibas.ch>
2015-02-06 11:31:47 +0100

    avoiding problem bewtween death and deathToHOH

--- src/main/java/org/openhds/mobile/model/UpdateEvent.java
+++ src/main/java/org/openhds/mobile/model/UpdateEvent.java
@@ -10,7 +10,7 @@ public class UpdateEvent {
 	public static final String INDIVIDUAL = "INDIVIDUAL";
 	public static final String VISIT = "VISIT";
 	public static final String PREGNANCYOUTCOME = "PREGNANCY_OUTCOME";
-	public static final String DEATH = "DEATH";
+	public static final String DEATH = "DEATH_REGISTRATION";
 	public static final String DEATHTOHOH = "DEATHTOHOH";
 	public static final String BASELINE = "BASELINE";
 	public static final String INMIGRATION = "IN_MIGRATION";
